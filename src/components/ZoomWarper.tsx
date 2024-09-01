ccf0e0ebdd182125bb1c721442144b5e52e757cf
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-09-02 00:57:30 +0700

    chore: remove unused imports

--- src/components/ZoomWarper.tsx
+++ src/components/ZoomWarper.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react"
 export const ZoomWrapper = ({ children }: { children: React.ReactNode }) => {
-
-    return (
-        <div className = "animate-zoom-in origin-center w-full h-full scale-150">
-            {children}
-        </div>
-    );
-}
+  return (
+    <div className="h-full w-full origin-center scale-150 animate-zoom-in">
+      {children}
+    </div>
+  );
+};

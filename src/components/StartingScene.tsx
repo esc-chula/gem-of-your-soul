a288fe639d8d9915f765401f0e9ef1126a1f5f01
wnmayy <wanathachat@gmail.com>
2024-09-10 15:03:50 +0700

    fix: change .png to .gif

--- src/components/StartingScene.tsx
+++ src/components/StartingScene.tsx
@@ -2,16 +2,25 @@ import { useEffect, useState } from "react";
 
 import Tap from "./Tap";
 
+import Image07 from "@/assets/story/scene-0/0-02.gif";
 import Image01 from "@/assets/story/scene-0/frame1.webp";
 import Image02 from "@/assets/story/scene-0/frame2.webp";
 import Image03 from "@/assets/story/scene-0/frame3.webp";
 import Image04 from "@/assets/story/scene-0/frame4.webp";
 import Image05 from "@/assets/story/scene-0/frame5.webp";
 import Image06 from "@/assets/story/scene-0/frame6.webp";
-import Image07 from "@/assets/story/scene-0/0-02.webp";
+
 
 const StartingScene = () => {
-  const images = [Image01, Image02, Image03, Image04, Image05, Image06, Image07];
+  const images = [
+    Image01,
+    Image02,
+    Image03,
+    Image04,
+    Image05,
+    Image06,
+    Image07,
+  ];
   const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
   const [lastImage, setLastImage] = useState<boolean>(false);
 

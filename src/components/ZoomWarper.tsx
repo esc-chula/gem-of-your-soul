da6d07585720f78a392fdb82a232b087dda2a7fd
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-09-10 15:11:15 +0700

    fix: client only

--- src/components/ZoomWarper.tsx
+++ src/components/ZoomWarper.tsx
@@ -1,7 +1,15 @@
+// export const ZoomWrapper = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <div className="h-full w-full origin-center scale-150 animate-zoom-in">
+//       {children}
+//     </div>
+//   );
+// };
+
 export const ZoomWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full w-full origin-center scale-150 animate-zoom-in">
-      {children}
+    <div className="h-full w-full origin-center">
+      <div className="h-full w-full scale-100">{children}</div>
     </div>
   );
 };

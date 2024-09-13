3ba2c5915c2c0c681ce7e4a56399f01670da07fc
Vijak Khajornritdacha <vijak13019@hotmail.com>
2024-09-13 21:46:29 +0700

    fix: fix web audit

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -6,17 +6,44 @@ import Tiktok from "@/assets/landing/tiktok.svg";
 export default function Footer() {
   return (
     <footer className="flex w-full items-center justify-center gap-3 border-t-[2px] border-black bg-base-03">
-      <img alt="larngear logo" src={LarngearLogo.src} />
+      <img alt="larngear logo" src={LarngearLogo.src} loading="lazy" />
       <p className="text-[20px] font-semibold text-black">LarnGear Camp</p>
       <div className="flex justify-center space-x-4">
-        <a href="#" aria-label="Facebook" className="text-gray-600">
-          <img className="aspect-square w-9" src={Facebook.src} />
+        <a
+          href="https://www.facebook.com/LARNGEARCAMP"
+          aria-label="Facebook"
+          className="text-gray-600"
+        >
+          <img
+            className="aspect-square w-9"
+            src={Facebook.src}
+            alt="Facebook logo"
+            loading="lazy"
+          />
         </a>
-        <a href="#" aria-label="Instagram" className="text-gray-600">
-          <img className="aspect-square w-9" src={InstagramBlack.src} />
+        <a
+          href="https://www.instagram.com/larngear_camp/"
+          aria-label="Instagram"
+          className="text-gray-600"
+        >
+          <img
+            className="aspect-square w-9"
+            src={InstagramBlack.src}
+            alt="Instagram logo"
+            loading="lazy"
+          />
         </a>
-        <a href="#" aria-label="TikTok" className="text-gray-600">
-          <img className="aspect-square w-9" src={Tiktok.src} />
+        <a
+          href="https://www.tiktok.com/@larngear_camp"
+          aria-label="TikTok"
+          className="text-gray-600"
+        >
+          <img
+            className="aspect-square w-9"
+            src={Tiktok.src}
+            alt="Tiktok logo"
+            loading="lazy"
+          />
         </a>
       </div>
     </footer>
